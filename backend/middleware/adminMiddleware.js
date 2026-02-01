1086ecc0b91cc4e60eeef31d4f35b98988fddf30
Sebastian Acha <melodico@gmail.com>
2026-02-01 10:53:03 -0300

    fix error en bd al escribir la config aun persiste 3

--- backend/middleware/adminMiddleware.js
+++ backend/middleware/adminMiddleware.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (req.user.rol !== 'admin') {
+    if (req.user.rol !== 'ADMIN') {
         return res.status(403).json({ message: 'Acceso denegado' });
     }
     next();
