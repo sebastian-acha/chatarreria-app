fd7dcaa10b613ec1e60863b5549b90731144edf6
Sebastian Acha <melodico@gmail.com>
2026-02-01 02:47:01 -0300

    js a jsx 2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ConfiguracionProvider } from './context/ConfiguracionContext.js'
+import { ConfiguracionProvider } from './context/ConfiguracionContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
