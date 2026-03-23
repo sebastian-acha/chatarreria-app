fc5d8fc8427a67370126f57a8fc7aaa918f034a4
Sebastian Acha <sebastian.acha@gmail.com>
2026-03-22 23:44:37 -0300

    Version

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <footer className="text-center py-4 mt-4 border-top">
             <p className="text-muted mb-0">Desarrollado por VentressLabs</p>
+            <p className="text-muted mb-0">Version: 1.1</p>
         </footer>
     );
 };
