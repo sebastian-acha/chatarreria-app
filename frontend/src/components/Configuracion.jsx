12fd909aebcc3f561d5a8a154c955f245dda7c16
Francisco Vásquez <fvasquezdg@gmail.com>
2026-04-01 19:38:55 -0300

    cambio DOM Configuración empresa

--- frontend/src/components/Configuracion.jsx
+++ frontend/src/components/Configuracion.jsx
@@ -57,64 +57,83 @@ const Configuracion = () => {
     };
 
     return (
-        <div className="container p-4">
-            <h1 className="h3 fw-bold mb-4">Configuración de la Empresa</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-4">
-                    <label className="form-label">Nombre de la Empresa</label>
-                    <input
-                        type="text"
-                        name="nombre_empresa"
-                        value={config.nombre_empresa}
-                        onChange={handleChange}
-                        className="form-control"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="form-label">Dirección</label>
-                    <input
-                        type="text"
-                        name="direccion"
-                        value={config.direccion}
-                        onChange={handleChange}
-                        className="form-control"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="form-label">Teléfono</label>
-                    <input
-                        type="text"
-                        name="telefono"
-                        value={config.telefono}
-                        onChange={handleChange}
-                        className="form-control"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="form-label">Email</label>
-                    <input
-                        type="email"
-                        name="email"
-                        value={config.email}
-                        onChange={handleChange}
-                        className="form-control"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="form-label">Logo</label>
-                    <input
-                        type="file"
-                        name="logo"
-                        onChange={handleLogoChange}
-                        className="form-control"
-                    />
-                    {config.logo_url && <img src={config.logo_url} alt="Logo" className="mt-4" style={{ height: '80px' }} />}
-                </div>
-                <button type="submit" className="btn btn-primary">
-                    Guardar Cambios
-                </button>
-            </form>
+    <div className="container my-4">
+      <div className="row justify-content-center">
+        <h2 className="h3 fw-bold text-center mb-4 gap-2">Configuración de la Empresa</h2>
+        <div className="col">
+          <div className="card shadow-sm">
+            <div className="card-body p-4">
+              <div className="box">
+                <form onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="mb-4 col-sm-6">
+                      <label className="form-label">Nombre de la Empresa</label>
+                      <input
+                          type="text"
+                          name="nombre_empresa"
+                          value={config.nombre_empresa}
+                          onChange={handleChange}
+                          className="form-control"
+                      />
+                    </div>
+                    <div className="mb-4 col-sm-6">
+                      <label className="form-label">Dirección</label>
+                      <input
+                          type="text"
+                          name="direccion"
+                          value={config.direccion}
+                          onChange={handleChange}
+                          className="form-control"
+                      />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="mb-4 col-sm-6">
+                      <label className="form-label">Teléfono</label>
+                      <input
+                          type="text"
+                          name="telefono"
+                          value={config.telefono}
+                          onChange={handleChange}
+                          className="form-control"
+                      />
+                    </div>
+                    <div className="mb-4 col-sm-6">
+                      <label className="form-label">Email</label>
+                      <input
+                          type="email"
+                          name="email"
+                          value={config.email}
+                          onChange={handleChange}
+                          className="form-control"
+                      />
+                    </div>
+                  </div>
+                  <div className="row justify-content-center">
+                    <div className="mb-4 col-sm-12 col-md-4">
+                      {config.logo_url && <img src={config.logo_url} alt="Logo" className="mt-4 mb-2" style={{ height: '80px'}} />}
+                      <input
+                          type="file"
+                          name="logo"
+                          onChange={handleLogoChange}
+                          className="form-control"
+                      />
+                    </div>
+                  </div>
+                  <div className="row justify-content-center">
+                    <div className="mb-4 col-sm-12 col-md-4">
+                      <button type="submit" className="btn btn-success w-100 d-flex justify-content-center">
+                          Guardar Cambios
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+    </div>
     );
 };
 
