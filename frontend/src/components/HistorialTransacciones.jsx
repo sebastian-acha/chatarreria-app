3c1721c415f321e55c854e079480ddb46d70ec6d
Francisco Vásquez <fvasquezdg@gmail.com>
2026-05-29 15:58:59 -0400

    orden voucher transacciones container

--- frontend/src/components/HistorialTransacciones.jsx
+++ frontend/src/components/HistorialTransacciones.jsx
@@ -150,7 +150,7 @@ const HistorialTransacciones = () => {
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
                     <style type="text/css">
-                        .container{padding:0;}
+                        .container{padding:0 6px;}
                         body {font-size:11px; font-family: tahoma, sans-serif; font-weight:100; letter-spacing: 0.02em;}
                         .font11{font-size:11px }
                         footer{font-size:10px;}
